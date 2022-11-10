2098d84d329c29608643d92caa16dde04c5d6ee2
adiwahyudinata <114970346+adiwahyudinata@users.noreply.github.com>
2022-11-10 19:27:47 +0800

    Add files via upload

---
+++ storage/framework/views/46d1434805e9275bf7884d19fd476ae4bf6fac76.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="resaurces/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Montserrat&display=swap" rel="stylesheet">
+
+
+
+
+</head>
+<body>
+   <nav>
+    <div class="logo">
+        <h3>ADI WAHYU SHOP</h3>
+    </div>
+
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Product</a></li>
+        <li><a href="">Kategory</a></li>
+        <li><a href="">Service</a></li>
+        <li><a href="">Galery</a></li>
+        <li><a href="">About</a></li>
+    </ul>
+
+    <div class="menu-toggle">
+        <input type="checkbox"/>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+   </nav> 
+
+   <script src="script.js"></script>
+
+
+   <div class="header">
+    <h1>SS<span>.id</span></h1>
+  </div>
+  <div class="tagline">
+    <p>Sebuah platform yang memberikan ulasan dan rekomendasi 
+        sepatu yang hits dan kekinian <strong>sesuai TREND</strong>.</p><br>
+    <input class="search-area" type="text" name="text" placeholder="Cari merk sepatu..." autocomplete="off">
+    <input class="search-btn" type="submit" name="submit" value="Search">
+  </div>
+  <div class="down">
+    <img src="asset/pngwing.com (3).png" alt="Logo.toko">
+  </div>
+</div>
+
+</body>
+</html><?php /**PATH D:\laravel\HomeEngine\resources\views/index.blade.php ENDPATH**/ ?>
